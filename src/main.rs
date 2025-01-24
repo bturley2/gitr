9789749acb25a6dcfa76167f53f1579896f4b5a0
Bryce Turley <turleyb2@gmail.com>
2025-01-24 08:40:06 -0800

    need to separate args and invocation logic

--- src/main.rs
+++ src/main.rs
@@ -23,14 +23,21 @@ impl Command {
 }
 
 fn main() {
+    // TODO separate argument parsing and command invocation
     parse_args();
-    // need method for identifying commands from CLI
 }
 
 // identifies commands given by the user, and invokes the appropriate logic
 fn parse_args() {
-    //let cmd = std::env::args().nth(1).expect("No command given!").as_str();
-    let cmd = String::from("init");
+    let err_msg = "Un-recognized command.";
+
+    let cmd = match std::env::args().nth(1) {
+        Some(cmd) => dbg!(cmd),
+        None => {
+            println!("{err_msg}");
+            return;
+        }
+    };
 
     match Command::new(&cmd) {
         Command::Init => cmd_init(),
@@ -39,18 +46,30 @@ fn parse_args() {
         Command::Push => cmd_push(),
         Command::Checkout => cmd_checkout(),
         Command::Switch => cmd_switch(),
-        Command::Invalid => println!("Un-recognized command."),
+        Command::Invalid => println!("{err_msg}"),
     }
 }
 
-fn cmd_init() {}
+fn cmd_init() {
+    println!("TODO")
+}
 
-fn cmd_add() {}
+fn cmd_add() {
+    println!("TODO");
+}
 
-fn cmd_commit() {}
+fn cmd_commit() {
+    println!("TODO");
+}
 
-fn cmd_push() {}
+fn cmd_push() {
+    println!("TODO");
+}
 
-fn cmd_switch() {}
+fn cmd_switch() {
+    println!("TODO");
+}
 
-fn cmd_checkout() {}
+fn cmd_checkout() {
+    println!("TODO");
+}
