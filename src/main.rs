61a984daafe61088b7e0ce7f3e242aeeb116528c
Bryce Turley <turleyb2@gmail.com>
2025-01-25 08:40:14 -0800

    reorged filels

--- src/main.rs
+++ src/main.rs
@@ -22,54 +22,58 @@ impl Command {
     }
 }
 
+// identifies which command to invoke & calls associated method
 fn main() {
-    // TODO separate argument parsing and command invocation
-    parse_args();
-}
-
-// identifies commands given by the user, and invokes the appropriate logic
-fn parse_args() {
-    let err_msg = "Un-recognized command.";
-
     let cmd = match std::env::args().nth(1) {
         Some(cmd) => dbg!(cmd),
         None => {
-            println!("{err_msg}");
+            println!("No command given.");
             return;
         }
     };
 
+    let args = parse_args();
+
     match Command::new(&cmd) {
-        Command::Init => cmd_init(),
-        Command::Add => cmd_add(),
-        Command::Commit => cmd_commit(),
-        Command::Push => cmd_push(),
-        Command::Checkout => cmd_checkout(),
-        Command::Switch => cmd_switch(),
-        Command::Invalid => println!("{err_msg}"),
+        Command::Init => cmd_init(args),
+        Command::Add => cmd_add(args),
+        Command::Commit => cmd_commit(args),
+        Command::Push => cmd_push(args),
+        Command::Checkout => cmd_checkout(args),
+        Command::Switch => cmd_switch(args),
+        Command::Invalid => println!("Un-recognized command."),
     }
 }
 
-fn cmd_init() {
+// identifies all additional command line arguments provided
+fn parse_args() -> Vec<String> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 2 {
+        return args[2..].to_vec();
+    }
+    return Vec::new();
+}
+
+fn cmd_init(_args: Vec<String>) {
     println!("TODO")
 }
 
-fn cmd_add() {
+fn cmd_add(_args: Vec<String>) {
     println!("TODO");
 }
 
-fn cmd_commit() {
+fn cmd_commit(_args: Vec<String>) {
     println!("TODO");
 }
 
-fn cmd_push() {
+fn cmd_push(_args: Vec<String>) {
     println!("TODO");
 }
 
-fn cmd_switch() {
+fn cmd_switch(_args: Vec<String>) {
     println!("TODO");
 }
 
-fn cmd_checkout() {
+fn cmd_checkout(_args: Vec<String>) {
     println!("TODO");
 }
